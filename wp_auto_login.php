c07f158dcadadb3732b382e24c7df07c10cdd3fa
Andry Gorokhovets <andrygorokhovets@users.noreply.github.com>
2016-09-19 13:27:38 +0300

    Update and rename .gitignore to wp_auto_login.php

---
+++ wp_auto_login.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'wp-load.php';
+
+$user_id = 1;
+$user = get_user_by( 'id', $user_id );
+if( $user ) {
+	wp_set_current_user( $user_id, $user->user_login );
+	wp_set_auth_cookie( $user_id );
+	do_action( 'wp_login', $user->user_login );
+	wp_redirect( 'wp-admin' );
+	exit;
+}
